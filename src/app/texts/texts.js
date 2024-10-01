52376a7a2e334b89985e06855f54081278c0c500
kevinpicon01 <digitalpruu@gmail.com>
2024-10-01 01:17:04 -0500

    Changed media, color, texts

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,9 +1,9 @@
-const title = "Titulo del emprendimiento";
-const email = "Email del emprendimiento";
+const title = "Donde Lucas";
+const email = "sanchezrodriguezcarmen9@gmail.com";
 const mensajeWhatsApp = encodeURIComponent(
-    `¡Hola! Estoy muy interesado en tus productos de ${title} y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!`
+    `¡Hola! Estoy interesado en los productos de ${title} y me gustaría recibir más información sobre los materiales de construcción que ofrecen. ¡Espero su respuesta!`
 );
-const numeroWhatsApp = "Numero del emprendimiento";
+const numeroWhatsApp = "3125448057";
 const textos = {
     index: {
         title: title,
@@ -12,25 +12,24 @@ const textos = {
         title: title,
     },
     about: {
-        description1: "Descripción del emprendimiento parrafo uno",
-        description2: "Descripcion del emprendimiento parrafo dos"
+        description1: "Donde Lucas es tu destino confiable para la compra de materiales de construcción como cemento, tubería y mucho más. Desde nuestro inicio, nos hemos comprometido a proporcionar productos de la más alta calidad, garantizando que cada compra cumpla con los estándares más exigentes.",
+        description2: "Nuestro objetivo es ofrecer soluciones completas para tus proyectos de construcción y remodelación, con productos que aseguren durabilidad y rendimiento. Trabajamos con proveedores de confianza para garantizar que cada producto que ofrecemos cumpla con las especificaciones y requisitos que necesitas.",
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
-        email: email
+        email: email,
     },
     footer: {
-        slogan:{
-            p1: "slogan del emprendimiento parrafo uno",
-            p2: "slogan del emprendimiento parrafo dos"
+        slogan: {
+            p1: "Cumplimiento y calidad en nuestros productos",
+            p2: "Tu aliado en la construcción",
         },
-        address: "Dirección del emprendimiento",
+        address: "",
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/",
+        facebook: "https://www.facebook.com/profile.php?id=61565696277822&mibextid=ZbWKwL",
         instagram: "https://www.instagram.com/",
-        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
+        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
     },
-
 };
 export default textos;
